8b97d357a0c566bdebc493ab326ec61827a1762b
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2015-02-03 21:12:53 +0700

    finish profile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,28 +1,74 @@
 // untuk taks manager
 var gulp    = require('gulp'),
-
 // untuk generate file css agar css mampu berjalan di semua browser dan memudahkan dalam develop css
 // mifify css
     sass    = require('gulp-ruby-sass'),
-
 // untuk memasukan/include file
     include = require('gulp-include'),
-
 // untuk minify css dan javascript (termasuk nama variable dan function)
-    uglify  = require('gulp-uglify');
+    uglify  = require('gulp-uglify'),
+// untuk minify, import, compatibility all browser and remove comment
+    minifyCSS = require('gulp-minify-css');
+
+var config = {
+    assetPath: __dirname+"/assets",
+    publicPath: __dirname+"/app/public",
+    bowerDir: __dirname+"/bower_components",
+}
+
+// memindahkan semua font ke public
+gulp.task('icons', function() { 
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
+        .pipe(gulp.dest(config.publicPath+'/fonts')); 
+});
+
+// Task Untuk generate custom.scss
+gulp.task("sass", function(){
+    return gulp.src(config.assetPath + '/sass/custom.scss')
+        .pipe(sass({
+            style: "compressed",
+            compass: true,
+            sourcemap: false,
+            lineNumbers: true,
+            check: true
+        }) 
+        .on("error", function(error){
+            console.log(error.message)
+        })) 
+        .pipe(gulp.dest(config.assetPath+'/lib_css')); 
+});
+
+// untuk import|menggabungkan|minify css
+gulp.task('css', function () {
+  gulp.src(config.assetPath+'/css/style.css')
+    .pipe(minifyCSS({
+        compatibility:'*',
+        debug:true,
+        keepBreaks:false,
+        keepSpecialComments:0
+    }))
+    .pipe(gulp.dest(config.publicPath+'/css'));
+});
 
 // Task Untuk Javascript
 gulp.task("script", function(){
-    gulp.src('assets/js/admin.js')
+    gulp.src(config.assetPath+'/js/main.js')
         .pipe(include())
         .pipe(uglify())
-        .pipe(gulp.dest('public/js'));
+        .pipe(gulp.dest(config.publicPath+'/js'));
+    gulp.src(config.assetPath+'/js/ie.js')
+        .pipe(include())
+        .pipe(uglify())
+        .pipe(gulp.dest(config.publicPath+'/js'));
 });
 
 // Watcher
 gulp.task('watch', function(){
-    gulp.watch('assets/js/**/*.js', ['script']);
+    gulp.watch(config.assetPath + '/js/**/*.js', ['script']);
+     gulp.watch(config.assetPath + '/sass/**/*.scss', ['sass']); 
+     gulp.watch(config.assetPath + '/lib_css/**/*.css', ['css']); 
+     gulp.watch(config.assetPath + '/css/**/*.css', ['css']); 
 });
 
 // Task Default
-gulp.task("default", ['script','watch']);
+gulp.task("default", ['script','css','sass','watch']);
---
+++ app/index.php
@@ -0,0 +1,255 @@
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Agus Nurwanto</title>
+    <meta name="description" content="Agus Nurwanto dari Magetan lahir pada 28 Agustus 1990">
+    <meta name="author" content="Agus Nurwanto">
+
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="public/css/style.css" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="public/images/laptop.png">
+
+</head>
+<body id="home" data-spy="scroll" data-target=".main-nav">
+
+  <!-- Preloader -->
+  <div id="preloader">
+    <div id="status">&nbsp;</div>
+  </div>
+
+  <div class="wrapper">
+
+    <!-- HEADER -->
+    <header id="header">
+      <div class="opacity-head"></div>
+      <div class="blurimg">
+        <div class="img-head"></div>
+        <div class="img-blur blur"></div>
+      </div>
+
+      <div class="col-xs-12 head-info">
+        <div>
+          <div class="wrapper-p">
+            <a href="#about" class="title-head home-des wow bounceInDown"></a>
+          </div>
+          <h1 class="desc-head home-des wow bounceInLeft">Agus Nurwanto</h1>
+          <h2 class="desc-head home-des wow bounceInLeft">Magetan, 28 Agustus 1990</h2>
+          <h3 class="sub-head home-des wow bounceInRight">Assalamualaikum Wr. Wb. Selamat datang teman, salam kenal, salam semangat!!! Semoga selalu gembira dan bersyukur jalani hari :)</h3>
+          <a href="#about" class="head-about wow bounceInUp">Selengkapnya</a>
+        </div>
+      </div>
+    </header>
+    <!-- End Header -->
+
+    <!-- Nav -->
+      <nav class="main-nav navbar-default" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-lampu-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="http://pondokprogrammer.com"><img src="public/images/pondokprogrammer.png" style="height:50px;"></a>
+            </div>
+
+            <div class="collapse navbar-collapse navbar-right navbar-lampu-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="menuItem"><a href="#home">Agus</a></li>
+                    <li class="menuItem"><a href="#about">Tentang</a></li>
+                    <li class="menuItem"><a href="#cita-cita">Harapan</a></li>
+                    <li class="menuItem"><a href="#hobi">Hobi</a></li>
+                    <li class="menuItem"><a href="#skill">Skill</a></li>
+                    <li class="menuItem"><a href="#contact">Alamat</a></li>
+                </ul>
+            </div>
+
+        </div>
+      </nav>
+    <!-- End Nav -->
+
+    <!-- MAIN CONTENT -->
+    <div class="main-content">
+
+      <!-- About -->
+      <section id="about">
+        <!-- Preview -->
+        <div class="preview">
+          <div class="container preview">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <h2 class="title-preview wow bounceIn">Tentang saya</h2>
+              <p class="des-preview  wow bounceInLeft">Saya lahir dari pasangan berbahagia bapak Marno (saat ini beliau tinggal di Magetan) dan ibu Marmi (beliau sudah almarhumah) di desa Genengan, Kecamatan Kawedanan, Kabupaten Magetan Jawa Timur pada tanggal 28 Agustus 1990. Saya nomor dua dari tiga bersaudara. Kakak saya Andik Nugroho (menikah dengan Kurnia, alhamdulillah sudah mendapatkan titipan anak perempuan dari Allah SWT yang diberi nama Adelia Novita Nugraha dan bekerja sebagai karyawan di Kabupaten Bogor Jawa Barat) lahir tiga tahun sebelum saya, tepatnya pada tanggal 1 April 1987 dan adik bernama Wahyu Setyo Marini (kuliah S1 di Yogjakarta bidang perhotelan sembari bekerja) lahir dua tahun setelah saya pada tanggal 14 November 1992. Selama masih bersekolah kami tinggal bersama di Magetan. Saya sekolah SD di SDN Genengan 2 Kecamatan Kawedanan sampai tahun 1998-2004. Kemudian dilanjutkan di SMPN Kawedanan 2 Kabupaten Magetan tanun 2004-2006 dan melanjutkan kembali ke SMKN 1 Bendo Kabupaten Magetan 2006-2008.</p>
+            </div>
+          </div>
+        </div>
+
+      </section>
+      <!-- End About -->
+
+      <!-- cita-cita -->
+      <section id="cita-cita">
+        <!-- Preview -->
+        <div class="preview">
+          <div class="container preview">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <h2 class="title-preview wow bounceInLeft">Harapan</h2>
+              <ul class="des-preview  wow bounceInRight list">
+                <li>Berharap besar mampu menghafalkan seluruh ayat-ayat alquran</li>
+                <li>Murojaah 1 juz alquran setiap hari</li>
+                <li>Selalu istiqomah menjalankan sunnah rosul Muhammad SAW.</li>
+                <li>Ikut berkontribusi mengembangkan pondok programmer menjadi salah satu pusat pendidikan terbaik yang menciptakan programmer handal baik secara teknis dan ahlaknya</li>
+                <li>Tahun 2017 sudah mampu menikah baik secara mental dan ekonomi</li>
+                <li>Berharap tahun 2020 mampu menunaikan haji</li>
+                <li>Menguasai prototype alur pemogramman linux</li>
+                <li>Intinya sih menjadi manusia yang banyak bermanfaat untuk sekitarnya.. :)</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </section>
+      <!-- End cita-cita -->
+
+      <!-- Hobi -->
+      <section id="hobi">
+        <!-- Preview -->
+        <div class="preview">
+          <div class="container preview">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <h2 class="title-preview wow bounceIn">Hobi</h2>
+              <p class="des-preview  wow bounceInLeft">Hobi saya adalah olahraga atau menjalankan hidup sehat. Segala hal yang menyehatkan adalah hobi saya. Kegiatan yang sekarang ini sering saya lakukan untuk menyalurkan hobi adalah bermain bola setiap hari selasa dan jum'at bersama anggota pondok programmer. Lari pagi setelah jam 05:15 WIB saat ada kesempatan. Termasuk juga belajar pemogramman adalah hobi saya, karena itu membuat otak berolahraga. Dalam dunia pemogramman hobi saya atau yang biasa disebut passion kalau di pondok programmer saat ini adalah mempelajari front end development karena banyak yang belum saya tau tentang konsep ini. "Bagaimana membuat halaman WEB yang user friendly dengan user experience yang baik?" adalah pertanyaan yang selalu ada di pikiran saya. Jika ada teman-teman sekalian yang memiliki minat yang sama seperti saya, mari kita belajar bersama. :)</p>
+            </div>
+          </div>
+        </div>
+
+      </section>
+      <!-- End Hobi -->
+
+      <!-- Skill -->
+      <section id="skill">
+        <!-- Preview -->
+        <div class="preview">
+          <div class="container preview">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <h2 class="title-preview wow bounceIn">Skill</h2>
+              <div class="col-md-6 col-sm-12 col-xs-6">
+                <table class="des-preview  wow bounceInRight list-t">
+                  <tbody>
+                    <tr>
+                      <td>HTML</td>
+                      <td>90%</td>
+                    </tr>
+                    <tr>
+                      <td>CSS</td>
+                      <td>90%</td>
+                    </tr>
+                    <tr>
+                      <td>PHP</td>
+                      <td>90%</td>
+                    </tr>
+                    <tr>
+                      <td>MYSQL</td>
+                      <td>90%</td>
+                    </tr>
+                    <tr>
+                      <td>JAVASCRIPT</td>
+                      <td>80%</td>
+                    </tr>
+                    <tr>
+                      <td>LINUX</td>
+                      <td>50%</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="col-md-6 col-sm-12 col-xs-6">
+                <table class="des-preview  wow bounceInRight list-t">
+                  <tbody>
+                    <tr>
+                      <td>API</td>
+                      <td>90%</td>
+                    </tr>
+                    <tr>
+                      <td>REDIS</td>
+                      <td>70%</td>
+                    </tr>
+                    <tr>
+                      <td>GULP</td>
+                      <td>70%</td>
+                    </tr>
+                    <tr>
+                      <td>UX</td>
+                      <td>20%</td>
+                    </tr>
+                    <tr>
+                      <td>NODE JS</td>
+                      <td>60%</td>
+                    </tr>
+                    <tr>
+                      <td>ELASTICSEARCH</td>
+                      <td>50%</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </section>
+      <!-- End Skill -->
+
+      <section id="contact">
+        <div class="address-contact">
+          <div class="container">
+            <ul>
+              <li><span class="wow bounceInLeft animated"><i class="fa fa-map-marker fa-lg"></i>Glondong No.10. Desa Wirokerten Kec. Banguntapan Kab. Bantul Yogjakarta</span></li>
+              <li><span class="wow bounceInRight animated"><i class="fa fa-envelope-o"></i>agus@pondokprogrammer.com</span></li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+    </div> <!-- main-content -->
+
+    <!-- FOOTER -->
+    <footer id="footer">
+      <div class="container">
+        <div class="main-sn clearfix">
+          <ul class="social-network">
+            <li><a href="https://www.facebook.com/agusnurwantomuslim" class="facebook-sn wow bounceInRight animated"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="https://twitter.com/jp_agusnurwanto" class="twitter-sn wow bounceInRight"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://plus.google.com/+AgusNurwanto/posts" class="google-sn wow bounceInDown"><i class="fa fa-google-plus"></i></a></li>
+            <li><a href="https://github.com/agusnurwanto" class="github-sn wow bounceInLeft"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://www.youtube.com/channel/UCtYfKy7If49QW9lOudjAarQ" class="google-sn wow bounceInLeft"><i class="fa fa-youtube-square"></i></a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="copyright clearfix">2014 &copy; &nbsp;theme from <a href='http://lamputhemes.com' class="lampu-p">lamputhemes</a>. Edited by Agus Nurwanto</div>
+
+    </footer>
+    <!-- End Footer -->
+
+  </div> <!-- End Wrapper -->
+
+  <script src="./public/js/main.js"></script>
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="./index_files/html5shiv.min.js"></script>
+  <script src="./index_files/respond.min.js"></script>
+  <![endif]-->
+  </body>
+</html>
